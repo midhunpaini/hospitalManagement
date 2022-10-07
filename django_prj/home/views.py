0091d18309093e770daf2d498e1329510b92e978
Midhun krishna P <midhunpk2@gmail.com>
2022-10-07 20:23:37 +0530

    hospital

---
+++ django_prj/home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    numbers = {
+        'num1' : [1,2,3,4,5,6,7,8,9,10],        
+    }  
+    return render(request, 'index.html', numbers)  
+
+def about(request):
+    return render(request, 'about.html')
+
+def booking(request):
+    return render(request, 'booking.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def doctors(request):
+    return render(request, 'doctors.html')
+
+def department(request):
+    return render(request, 'department.html')
